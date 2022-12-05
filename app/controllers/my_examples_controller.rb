73c1415c050c9225d47e457fafd05428c5896388
JRisJunior <jeffmrubin@gmail.com>
2022-12-05 11:44:21 -0600

    add random fortune to def roll in rng get

--- app/controllers/my_examples_controller.rb
+++ app/controllers/my_examples_controller.rb
@@ -1,5 +1,8 @@
 class MyExamplesController < ApplicationController
+  
+
   def roll
-    render json: {message: "hello"}
+    random_fortune = ["You have good luck!", "You have bad luck!", "You have AWFUL luck!"].sample
+    render json: {message: "#{random_fortune}"}
   end
 end
